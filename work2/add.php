ffa6d7e9afb4d77645f6e3c0993e3e44bf1e7aeb
BekkiHaruka <muses-c.8823@docomo.ne.jp>
2018-02-08 11:46:34 +0900

    １回目

---
+++ work2/add.php
@@ -0,0 +1,31 @@
+<html>
+<meta charset="utf-8">
+<head>
+<title>はてなユーザー登録</title>
+</head>
+<body>
+<tr>
+<td>はてなのサービスをご利用いただくには、ユーザー登録(無料)をする必要があります。</td>
+</tr>
+<br>
+<br>
+<form action="insert.php" method="POST">
+<table>
+<tr>
+<td>はてなID</td>
+</tr>
+<tr>
+<td><input type="text" name="name"></td>
+</tr>
+<tr>
+<td>パスワード</td>
+</tr>
+<tr>
+<td><input type="text" name="password"></td>
+</tr>
+</table>
+<br>
+<input type="submit" value="入力内容を確認">
+</form>
+</body>
+</html>
